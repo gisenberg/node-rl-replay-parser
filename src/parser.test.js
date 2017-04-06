fa8043e13fbd2fb597facbf8f0b8f84be1e33ebb
Gabriel Isenberg <gisenberg@gmail.com>
2017-04-05 23:23:55 -0700

    Revise tests.

--- src/parser.test.js
+++ src/parser.test.js
@@ -4,28 +4,30 @@ import Parser from './parser';
 import { expect } from 'chai';
 
 describe('parser', () => {
+  let replayFile;
   let replaySnapshot;
 
   beforeEach(() => {
-    replaySnapshot = fs.readFileSync(path.join(__dirname, './snapshots/1.replay'));
+    replayFile = fs.readFileSync(path.join(__dirname, './snapshots/1.replay'));
+    replaySnapshot = require('./snapshots/1.js');
   });
 
   describe('parseHeader', () => {
     it('should parse the header', () => {
-      // const parser = new Parser();
-      // const replayHeader = parser.parseHeader(replaySnapshot);
-      //
-      // expect(true).to.eql(false);
+      const parser = new Parser();
+      const replayHeader = parser.parseHeader(replayFile);
+
+      expect(replayHeader).to.eql(replaySnapshot.Header);
     });
   });
 
   describe('parse', () => {
       it('should parse the CRC and version number', () => {
         const parser = new Parser();
-        const replay = parser.parse(replaySnapshot);
+        const replay = parser.parse(replayFile);
 
-        expect(replay.CRC).to.eql('59f93396');
-        expect(replay.Version).to.eql('868.17');
+        expect(replay.CRC).to.eql(replaySnapshot.CRC);
+        expect(replay.Version).to.eql(replaySnapshot.Version);
       });
   });
 });
